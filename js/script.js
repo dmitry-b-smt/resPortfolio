198b889a3e372c10c6de80b0f79d2b14e8142fc6
dmitry-b-smt <84676033+dmitry-b-smt@users.noreply.github.com>
2021-12-01 23:33:40 +0300

    Add project-tree

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// 'use strict' // включаем строгий режим
+
+// let arr = [
+//     'Niel',
+//     'Ivan',
+//     'Bono',
+//     'Ishtwan',
+//     'Olya',
+// ];
+
+
+// let arrTwo = arr.slice(0, 1);
+// console.log(arrTwo);
+
+// let result = arr.map((item, index, array) => item[0]);
+
+// console.log(result);
+
+// console.log(Array.isArray(arr));
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// let arrNum = [1, 2, 3, 4,];
+// let resultOne = arrNum.reduce(function(previousValue, item, index, array) {
+//     return previousValue + item;
+// }, 0);
+
+// console.log(resultOne);
+
+// let resultTwo = arr.reduce(function(previousValue, item, index, array) {
+//     console.log(previousValue);
+//     console.log(item);
+//     return `${item}, ${previousValue}`;
+// });
+// console.log(resultTwo);
+
+// let arr = ['Ваня', 'Иштван', 'Оля',];
+// let removed = arr.splice(1, 1);
+// console.log(removed);
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// };
+
+// let str = 'Ваня,Иштван,Оля';
+// let arrr = str.split(',');
+// console.log(arrr);
+
+// let arr = [9, 2, 8,];
+// let reduceValue = arr.reduce(function(previousValue, item, index, array) {
+//     return (previousValue + item);
+// })
+
+// console.log(reduceValue);
